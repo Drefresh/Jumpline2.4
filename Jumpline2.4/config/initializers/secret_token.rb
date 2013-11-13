26d255b7c695eb0ab308a7a48ff0c06dbc303ce9
Andre Salmon <Salmon.andre.c@gmail.com>
2013-11-13 18:27:54 -0500

    initial push

---
+++ Jumpline2.4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jumpline24::Application.config.secret_key_base = '9de4bb7c5725b21358f50f621bb8a842d3e5032c6b8e68ba9fc0907617e4dfd78be8d46ebd80cef38f41722894e11af0d35e9b7a1657a33cb58d8048ddfc0f95'
---
+++ Jumpline2.4/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jumpline24::Application.config.session_store :cookie_store, key: '_Jumpline2_4_session'
