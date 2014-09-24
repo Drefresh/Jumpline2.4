d1ae126bfe4fd6a5c812286ddf039b7e213c1f6c
Andre Salmon <Salmon.andre.c@gmail.com>
2014-09-24 13:10:00 -0400

    Implemented Devise for users

--- Jumpline2.4/config/environments/production.rb
+++ Jumpline2.4/config/environments/production.rb
@@ -77,4 +77,9 @@ Jumpline24::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for Heroku#
+  #remember to set as actual host#
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
